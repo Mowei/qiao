a4e0c28b169c5a4fc25e1a591d6e9d9784807110
Moti <motitseng@utron.com.tw>
2017-09-08 00:34:13 +0800

    add UpgradeStatus flag

--- config.lua
+++ config.lua
@@ -6,6 +6,7 @@ M.SSID = {}
 M.SSID["WiFi-1"] = "Pass"
 M.SSID["WiFi-2"] = "Pass"
 
+M.UpgradeStatus = "NO"
 M.ScriptUrl = "https://raw.githubusercontent.com/Mowei/qiao/master/"
 
 M.MyTopic = "TEST/MyTopic"
